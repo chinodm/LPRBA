5529683d646c4ce09fc300d78bb12e54a73bf99c
Operadores de AS400 <Operaciones@osseg.local>
2022-08-23 10:35:42 -0300

    NavBar

---
+++ lprba/src/componentes/NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import style from 'style-components'
+
+function NavBar() {
+  return (
+    <>
+    <NavContainer>
+        <h2>La Pelu <span>by Ailu</span></h2>
+
+        </NavContainer>
+    </>
+  );
+}
+
+export default NavBar;
+
+
